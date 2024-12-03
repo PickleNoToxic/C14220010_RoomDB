c9dda9cb46219ac895503370c115599b7f351998
PickleNoToxic <matthew.kevin03@gmail.com>
2024-12-03 19:39:18 +0700

    Initial commit

---
+++ app/src/main/java/uts/c14220010/c14220010_roomdb/TambahDaftar.kt
@@ -0,0 +1,80 @@
+package uts.c14220010.c14220010_roomdb
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import uts.c14220010.c14220010_roomdb.database.daftarBelanja
+import uts.c14220010.c14220010_roomdb.database.daftarBelanjaDB
+import uts.c14220010.c14220010_roomdb.helper.DateHelper.getCurrentDate
+
+class TambahDaftar : AppCompatActivity() {
+    var DB = daftarBelanjaDB.getDatabase(this)
+    var tanggal = getCurrentDate()
+
+    var iID : Int = 0
+    var iAddEdit : Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tambah_daftar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val _btnTambah = findViewById<Button>(R.id.btnTambah)
+        val _btnUpdate = findViewById<Button>(R.id.btnUpdate)
+        val _etItem = findViewById<EditText>(R.id.etItem)
+        val _etJumlah = findViewById<EditText>(R.id.etJumlah)
+        _btnTambah.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).async {
+                DB.fundaftarBelanjaDAO().insert(
+                    daftarBelanja(
+                        tanggal = tanggal,
+                        item = _etItem.text.toString(),
+                        jumlah = _etJumlah.text.toString()
+                    )
+                )
+            }
+            finish()
+        }
+
+        _btnUpdate.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).async {
+                DB.fundaftarBelanjaDAO().update(
+                    isi_tanggal = tanggal,
+                    isi_item = _etItem.text.toString(),
+                    isi_jumlah = _etJumlah.text.toString(),
+                    pilihid = iID)
+            }
+            finish()
+        }
+
+        iID = intent.getIntExtra("id", 0)
+        iAddEdit = intent.getIntExtra("addEdit", 0)
+        if (iAddEdit == 0) {
+            _btnTambah.visibility = View.VISIBLE
+            _btnUpdate.visibility = View.GONE
+            _etItem.isEnabled = true
+        } else {
+            _btnTambah.visibility = View.GONE
+            _btnUpdate.visibility = View.VISIBLE
+            _etItem.isEnabled = false
+            CoroutineScope(Dispatchers.IO).async {
+                val item = DB.fundaftarBelanjaDAO().select(iID)
+                _etItem.setText(item.item)
+                _etJumlah.setText(item.jumlah)
+            }
+        }
+    }
+}
